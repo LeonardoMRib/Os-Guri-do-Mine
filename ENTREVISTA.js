6065e4998b7c461b5e2867b349e615c5765d9779
LeonardoMRib <108944988+LeonardoMRib@users.noreply.github.com>
2024-08-28 08:58:10 -0300

    Add files via upload

---
+++ ENTREVISTA.js
@@ -0,0 +1,27 @@
+let entrevista_1 = {
+    entrevistador: "Jesus",
+    entrevistado: "Emanuel",
+    questoes: {
+        p1: "Qual é seu nome e sua idade?",
+        r1: ";",
+        p2: "Qual era seu sonho profissional quando pequeno?",
+        r2: "",
+        p3: "Qual é o seu sonho profissional hoje?",
+        r3: ",
+        p4: "Qual é o seu filme favorito atualmente?",
+        r4: "",
+        p5: "Quais são seus melhores amigos hoje em dia?",
+        r5: "",
+        p6: "Qual é o seu tipo de esporte favorito?",
+        r6: "",
+        p7: "Você gostaria de seguir uma carreira profissional relacionada a esportes?",
+        r7: "",
+        p8: "Futuramente, você pretende viajar para algum lugar?",
+        r8: "",
+        p9: "Se você tivesse a oportunidade de escolher onde você iria estudar? Onde seria(em qualquer lugar)? E qual curso?",
+        r9: "",
+        p10: "Se você estivesse em um poço dos desejos? O que você iria desejar? Por quê?",
+        r10: "",
+    }
+
+}
